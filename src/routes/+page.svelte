e2b6222b62c828325fbbade6153c8d6845d3607d
ThisSilenceIsMine <aurakel542@gmail.com>
2024-03-27 17:49:28 +0200

    Base svelte page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
+<script lang="ts">
+	import Gun from 'gun';
+	const gun = Gun();
+
+	const sharedTextNode = gun.get('text');
+
+	let textValue = '';
+
+	sharedTextNode.on((data) => {
+		console.log('new data', data);
+		textValue = data.text;
+	});
+
+	$: if (textValue) {
+		sharedTextNode.put({ text: textValue });
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<textarea bind:value={textValue}></textarea>
