e8556047ab0d8b863ab196217744d7decd25d7c9
ThisSilenceIsMine <aurakel542@gmail.com>
2024-06-03 22:09:40 +0300

    chore: Update Edit and Render components for text node editing

---
+++ src/components/Node/Text/Edit.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Page } from '$lib/Page';
+  import { type Node } from '$lib/Node/node';
+	import type { FormEventHandler } from 'svelte/elements';
+
+  export let path = '';
+  export let key = '';
+
+  const page = new Page(path);
+
+  let self: Node | null = null;
+  
+  $: if(key) {
+    page.trackNode(key, (node) => {
+      console.log('node change', {node})
+      self = node;
+    });
+  }
+
+  const setContent = (value: string) => {
+    if (self) {
+      page.updateNode(self.key, {value})
+    }
+  };
+
+  const onInput: FormEventHandler<HTMLTextAreaElement> = (e) => {
+    setContent((e.target as HTMLTextAreaElement).value);
+  };
+
+  $: value = self?.value ?? '';
+</script>
+
+<div class="card">
+  <p class="card-title">{self?.key}</p>
+  <textarea class="textarea textarea-bordered" value={value} on:input={onInput}></textarea>
+</div>
---
+++ src/components/Node/Text/Render.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { Page } from '$lib/Page';
+  import { type Node } from '$lib/Node/node';
+
+  export let path = '';
+  export let key = '';
+
+  const page = new Page(path);
+
+  let self: Node | null = null;
+
+  page.trackNode(key, (node) => {
+    self = node;
+  });
+
+  $: value = self?.value ?? '';
+
+</script>
+
+
+<div class="p-4 shadow rounded">
+  <p class="text-gray-700">{value}</p>
+</div>
