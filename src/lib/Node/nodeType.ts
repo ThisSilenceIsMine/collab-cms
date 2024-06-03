597547aae46788656957ad95003463f2a6de2aae
ThisSilenceIsMine <aurakel542@gmail.com>
2024-06-03 23:27:28 +0300

    chore: Comment-out breadcrumbs node in nodeType.ts

--- src/lib/Node/nodeType.ts
+++ src/lib/Node/nodeType.ts
@@ -3,7 +3,7 @@ export type NodeType = (typeof NodeType)[keyof typeof NodeType];
 export const NodeType = {
 	text: 'text',
 	title: 'title',
-	breadcrumb: 'breadcrumbs',
+	// breadcrumb: 'breadcrumbs', TODO: Implement breadcrumbs node
 	//image: 'image', TODO: Implement image node
 	link: 'link'
 } as const;
