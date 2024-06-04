5a2b1dad87bd23ead282721d5aadecbc3ed653ad
ThisSilenceIsMine <aurakel542@gmail.com>
2024-06-04 15:10:46 +0300

    chore: Refactor Edit.svelte to use createTrackNodeStore for managing node state and content

--- src/components/Node/Text/Edit.svelte
+++ src/components/Node/Text/Edit.svelte
@@ -2,31 +2,31 @@
 	import { Page } from '$lib/Page';
 	import { type Node } from '$lib/Node/node';
 	import type { FormEventHandler } from 'svelte/elements';
+	import { createTrackNodeStore } from '../../../lib/Node/createNodeStore';
+	import { onMount } from 'svelte';
 
 	export let path = '';
 	export let key = '';
 
-	const page = new Page(path);
+	const { self: store, debouncedSetContent } = createTrackNodeStore(path, key);
 
 	let self: Node | null = null;
 
-	$: if (key) {
-		page.trackNode(key, (node) => {
+	onMount(() => {
+		const unsubscribe = store.subscribe((node) => {
 			self = node;
 		});
-	}
 
-	const setContent = (value: string) => {
-		if (self) {
-			page.updateNode(self.key, { value });
-		}
-	};
+		return () => {
+			unsubscribe();
+		};
+	});
 
 	const onInput: FormEventHandler<HTMLTextAreaElement> = (e) => {
-		setContent((e.target as HTMLTextAreaElement).value);
+		debouncedSetContent((e.target as HTMLTextAreaElement).value);
 	};
 
-	$: value = self?.value ?? '';
+	$: value = self?.value;
 </script>
 
 <div class="card">
