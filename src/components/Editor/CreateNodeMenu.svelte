272a5482d19e8b2c9905960bf18d2fed89a37819
ThisSilenceIsMine <aurakel542@gmail.com>
2024-06-04 14:43:31 +0300

    chore: Add CreateNodeMenu component for creating new elements in the Editor

---
+++ src/components/Editor/CreateNodeMenu.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { NodeType } from '../../lib/Node/nodeType';
+
+	const nodeTypes = Object.keys(NodeType);
+
+	const dispatch = createEventDispatcher();
+
+	function createNode(type: string) {
+		dispatch('createNode', { type });
+	}
+</script>
+
+<details class="dropdown">
+	<summary class="m-1 btn">Create new element</summary>
+	<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+		{#each nodeTypes as type}
+			<li>
+				<a on:click={createNode.bind(null, type)}>{type}</a>
+			</li>
+		{/each}
+	</ul>
+</details>
