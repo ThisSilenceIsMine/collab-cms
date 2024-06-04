5401179c04cb097647320bbbc254655c5421feb5
ThisSilenceIsMine <aurakel542@gmail.com>
2024-06-04 16:56:23 +0300

    feat: Create generic editor page

---
+++ src/routes/editor/[slug].svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Editor from '../../components/Editor/Editor.svelte';
+
+	const path = $page.params.slug;
+</script>
+
+<Editor {path} />
